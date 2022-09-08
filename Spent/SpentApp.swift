4b68a296a5a3e92a33bb774cd6a5eaeb2f41bfae
Sajjad <sajad.abedi@veriff.net>
2022-09-08 17:02:20 +0300

    Initial Commit

---
+++ Spent/SpentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpentApp.swift
+//  Spent
+//
+//  Created by Sajad Abedi on 08.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct SpentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
