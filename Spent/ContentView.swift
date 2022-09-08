6c53f3aba0901276f2e01ff61e632cfa3ba40e0c
Sajjad <sajad.abedi@veriff.net>
2022-09-08 17:16:52 +0300

    first commit

--- Spent/ContentView.swift
+++ Spent/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Spend app")
-            .padding()
+        HStack {
+            Image(systemName: "xmark")
+            Text("Spend app")
+                .padding()
+        }
     }
 }
 
