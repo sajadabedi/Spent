b796ab47bc1c7f3158a6ca6ce532f6e8072e1c48
Sajjad <sajad.abedi@veriff.net>
2022-09-11 19:12:50 +0300

    added font icon library

--- Spent/TransactionRow.swift
+++ Spent/TransactionRow.swift
@@ -6,11 +6,19 @@
 //
 
 import SwiftUI
+import SwiftUIFontIcon
 
 struct TransactionRow: View {
     var transaction: Transaction
     var body: some View {
         HStack(spacing:20) {
+            // MARK: Transaction Icon
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .fill(Color.icon.opacity(0.3))
+                .frame(width: 44, height: 44)
+                .overlay{
+                    FontIcon.text(.awesome5Solid(code: .icons), fontsize: 24, color: Color.icon)
+                }
             VStack(alignment: .leading, spacing: 6){
                 // MARK: Transaction Merchant
                 Text(transaction.merchant)
