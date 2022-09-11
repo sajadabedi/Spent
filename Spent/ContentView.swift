a5b650de9ade0a86c3af3ee3424508e420d4bd32
Sajjad <sajad.abedi@veriff.net>
2022-09-11 18:34:08 +0300

    Added colors and navigationView

--- Spent/ContentView.swift
+++ Spent/ContentView.swift
@@ -10,14 +10,39 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            Text("Spend app")
-                .padding()
+            NavigationView {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 24){
+                        Text("Overview")
+                            .font(.title2)
+                            .fontWeight(.bold)
+                    }
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                }
+                .background(Color.background)
+                .toolbar {
+                    // MARK: Notification Icon
+                    ToolbarItem {
+                        Image(systemName: "bell.badge")
+                            .symbolRenderingMode(.palette)
+                            .foregroundStyle(Color.icon, .primary)
+                    }
+                }
+            }
+            .navigationViewStyle(.stack)
+            .navigationBarTitleDisplayMode(.inline)
+            
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
---
+++ Spent/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  Spent
+//
+//  Created by Sajad Abedi on 11.09.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
